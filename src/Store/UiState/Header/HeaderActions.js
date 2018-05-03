9f8665bb5d0ae20942ae02397fe7c9ae3fa3c2aa
GALOEA <eddyhernandez0921@live.com>
2018-05-03 14:20:07 -0500

    console log clean

--- src/Store/UiState/Header/HeaderActions.js
+++ src/Store/UiState/Header/HeaderActions.js
@@ -1,7 +1,6 @@
 import headerConsts from './HeaderConstants';
 
 export const setSearchValue = (value) => {
-  console.log(value, 'HeaderActions.js 4 ');
   return { type: headerConsts.SET_SEARCH_VALUE, payload: value }
 }
 export default {
--- src/Store/UiState/Header/HeaderReducer.js
+++ src/Store/UiState/Header/HeaderReducer.js
@@ -3,7 +3,6 @@ import headerModel from './HeaderModel';
 const HeaderReducer = (state = headerModel, action) => {
   switch (action.type) {
     case headerConsts.SET_SEARCH_VALUE:
-      console.log(action.payload, 'HeaderReducer.js 6 ');
       return { ...state, searchValue: action.payload }
     default:
       return state;
