a36c9db831d1533b11de520a611149b611242aee
GALOEA <eddyhernandez0921@live.com>
2018-05-10 07:45:05 -0500

    fix polyfills

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import configureStore from './Store/configureStore';
 import App from './Components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import './polyfills';
 const store = configureStore();
 ReactDOM.render(
   <Provider store={store}>
