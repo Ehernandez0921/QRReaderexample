926b6445453d6d7a787607d0954ed75a26dd56d0
GALOEA <eddyhernandez0921@live.com>
2018-08-27 14:25:09 -0500

    fixed api call

--- src/Store/DataState/App/AppConsts.js
+++ src/Store/DataState/App/AppConsts.js
@@ -1,6 +1,6 @@
 export const appName = "Default App"
 export const SET_APP_SETTINGS = 'SET_APP_SETTINGS';
-export const API = process.env.NODE_ENV === 'production' ? 'http://vrtx.dow.com' : 'http://vrtx.dow.com/';
+export const API = process.env.NODE_ENV === 'production' ? 'http://vrtx.dow.com/' : 'http://vrtx.dow.com/';
 export default {
   appName,
   SET_APP_SETTINGS,
