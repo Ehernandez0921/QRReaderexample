7d321979316d968aa1e92fd41b8a7995bb4b88fe
GALOEA <eddyhernandez0921@live.com>
2018-08-27 07:49:13 -0500

    added spread item to tree node

--- src/Components/DowTreeList/DowTreeList.js
+++ src/Components/DowTreeList/DowTreeList.js
@@ -1,24 +1,25 @@
-import React,{Component}from 'react';
-import {Tree} from 'antd';
+import React, { Component } from 'react';
+import { Tree } from 'antd';
 
-const {TreeNode} = Tree;
+const { TreeNode } = Tree;
 
-export default class extends Component{
+export default class extends Component {
     loop = data => data.map((item) => {
         if (item.children && item.children.length) {
-          return <TreeNode key={item.key} title={item.title}>{this.loop(item.children)}</TreeNode>;
+            return <TreeNode {...item} key={item.key} title={item.title} dataRef={item}>{this.loop(item.children)}</TreeNode>;
         }
-        return <TreeNode key={item.key} title={item.title} />;
-      });
-    render(){
+        return <TreeNode {...item} key={item.key} title={item.title} dataRef={item} />;
+    });
+    render() {
         return (
             <Tree
-            loadData={this.props.onLoadData?this.props.onLoadData:null}
-            showLine
-            draggable
-            {...this.props.treeOptions}
+                loadData={this.props.onLoadData?this.props.onLoadData:null}
+                showLine
+                draggable
+                // onSelect={this.props.onSelect}
+                {...this.props.treeOptions}
             >
-            {this.props.data && this.loop(this.props.data)}
+                {this.props.data && this.loop(this.props.data)}
             </Tree>
         )
     };
