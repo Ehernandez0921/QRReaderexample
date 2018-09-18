b6fa55bd1738452c02f2b9ee25338e57de6a71d2
GALOEA <eddyhernandez0921@live.com>
2018-09-18 15:32:56 -0500

    added options to select in dowdetails

--- src/Components/DowDetails/DowField.js
+++ src/Components/DowDetails/DowField.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { Form, Icon, Input, Button, DatePicker, Select } from 'antd';
 const FormItem = Form.Item;
+const { Option } = Select;
 class DowField extends Component {
   render() {
-    const { title, name, fieldType, rule = [] } = this.props;
+    const { title, name, fieldType, rule = [], items } = this.props;
     switch (fieldType) {
       case 'date':
         return <DatePicker />;
       case 'select':
-        return <Select {...this.otherProps} rule={rule} />
+        const options = this.state.data.map(d => <Option key={d.value}>{d.text}</Option>);
+        return <Select {...this.otherProps} rule={rule} >{options}</Select>
       case 'input':
       default:
         return <Input />;
