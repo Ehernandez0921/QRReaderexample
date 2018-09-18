9c284da5b0f268407859513db7c622b72358f00b
GALOEA <eddyhernandez0921@live.com>
2018-09-18 14:34:27 -0500

    added Date Type to DowDetails

--- src/Components/DowDetails/DowField.js
+++ src/Components/DowDetails/DowField.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import { Form, Icon, Input, Button } from 'antd';
+import { Form, Icon, Input, Button, DatePicker } from 'antd';
 const FormItem = Form.Item;
 class DowField extends Component {
   render() {
     const { title, name, fieldType, rule = [] } = this.props;
     switch (fieldType) {
+      case 'date':
+        return <DatePicker />;
       case 'input':
       default:
-        return <Input />
+        return <Input />;
     }
   }
 }
