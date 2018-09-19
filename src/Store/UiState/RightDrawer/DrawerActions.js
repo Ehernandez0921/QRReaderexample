e242ef3b218037c6890a729e08a498a9634279a2
GALOEA <eddyhernandez0921@live.com>
2018-09-19 07:15:29 -0500

    fixed right panel reducer

--- src/Store/UiState/RightDrawer/DrawerActions.js
+++ src/Store/UiState/RightDrawer/DrawerActions.js
@@ -1,6 +1,6 @@
 import drawerConsts from "./DrawerConstants";
 export const toggleDrawer = show => {
-  return { type: drawerConsts.TOGGLE_DRAWER, payload: { show } }
+  return { type: drawerConsts.TOGGLE_RIGHT_DRAWER, payload: { show } }
 }
 export const setDrawerModel = model => {
   return {
--- src/Store/UiState/RightDrawer/DrawerReducer.js
+++ src/Store/UiState/RightDrawer/DrawerReducer.js
@@ -2,7 +2,8 @@ import drawerConsts from "./DrawerConstants";
 import drawerModel from './DrawerModel';
 const DrawerReducer = (state = drawerModel, action) => {
   switch (action.type) {
-    case drawerConsts.TOGGLE_DRAWER:
+    case drawerConsts.TOGGLE_RIGHT_DRAWER:
+    console.log( 'DrawerReducer.js Line-6')
       return { ...state, ...action.payload }
     case drawerConsts.SET_DRAWER_MODEL:
       return { ...state, model: action.payload }
