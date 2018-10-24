9596f9ed4a4c026caf41209560e50451294c1881
Eddy Hernandez <U759926@DOW.COM>
2018-10-23 19:58:15 -0500

    added server.js

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -1,7 +1,8 @@
 export { default as exportUtils, exportToFile } from './exportUtils';
 export const fetchBarcodeData = async (barcode) => {
+    console.log( 'utils.js Line-3');
     try {
-        const response = await fetch(`/product/${barcode}/70F7A52D147BABF68FA32CD9D6B01A0A`);
+        const response = await fetch(`/searchCode/${barcode}`);
         const data = await response.json();
         return data;
     } catch (error) {
--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var app = express();
+var fetch=require('node-fetch');
+// app.use(express.static(__dirname + '/publicServer'));
 
-app.use(express.static(__dirname + '/publicServer'));
-
-app.get('/fimlList', function(req, res) {
-  console.log('i receive a GET request');
-
-  var tryFetch = {myString: 'I am working fetch'};
-
-  res.json(tryFetch)
+app.get('/searchCode/:codeId', async function (req, res) {
+  try {
+    const response = await fetch(`https://api.upcdatabase.org/product/${req.params.codeId}/70F7A52D147BABF68FA32CD9D6B01A0A`);
+    const data=await response.json();
+    res.json(data)
+  } catch (err) {
+    res.json(err);
+  }
 })
 
-app.listen(3000);
+app.listen(process.env.PORT || 8080, () => { console.log('heere') });
 console.log('Server running on port 3000');
